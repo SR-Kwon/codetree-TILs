cf9cc04f59571c865ef1290ed89462f0ca4b74e8
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-09 20:54:30 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240709/자연수면 출력/output-only-natural-number.cpp
+++ 240709/자연수면 출력/output-only-natural-number.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main() {
     int a,b;
     cin >> a >> b;
-    if(a <= 0) return -1;
+    if(a <= 0){
+        cout << 0; 
+        return 0;
+    }
     for(int i = 0; i < b; i++){
         cout << a;
     }
