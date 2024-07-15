fd617425bb5751dac0804a6564f7a473f3af9edb
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-15 15:21:45 +0900

    Title: 신기한 타일 뒤집기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240715/신기한 타일 뒤집기/strange-flipping-tiles.cpp
+++ 240715/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //white = -1;
 //black = 1;
 
-vector<int> tiles(8001);
+vector<int> tiles(9001);
 
 int R(int step, int pointer){
     for(int i = 0; i < step; i++){
@@ -25,7 +25,7 @@ int L(int step, int pointer){
 }
 
 int main() {
-    int dir_num, step, pointer = 4000;
+    int dir_num, step, pointer = 4500;
     char direction;
     cin >> dir_num;
 
