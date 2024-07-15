80b18b7568eabfbf17f4d2dee1151907b53f2b70
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-15 12:45:36 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/비오는 날/rainy-day.cpp
+++ 240715/비오는 날/rainy-day.cpp
@@ -5,25 +5,18 @@ using namespace std;
 int main() {
     
     int n;
-    vector<vector<string>> rain;
-    vector<vector<string>> rain_date;
-
     cin >> n;
+
+    vector<string> min = {"9999-99-99", " ", " "};
 
     for(int i = 0; i < n; ++i){
         string date, day, weather;
         cin >> date >> day >> weather;
 
-        if(weather == "Rain"){
-            vector<string> temp1 = {date, day, weather};
-            rain.push_back(temp1);
+        if(weather == "Rain" && min[0] > date){
+            vector<string> temp = {date, day, weather};
+            min = temp;
         }
-    }
-    
-    vector<string> min = {"9999-99-99", " ", " "};
-
-    for(auto& i : rain) {
-        if(i[0] < min[0]){min = i;}
     }
 
     cout << min[0] << ' ' << min[1] << ' ' << min[2] << endl;
