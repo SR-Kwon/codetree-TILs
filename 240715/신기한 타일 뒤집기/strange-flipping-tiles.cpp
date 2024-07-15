cc1fb3e8da559d72f98ed9a13d9fd88e832b35fd
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-15 15:18:03 +0900

    Title: 신기한 타일 뒤집기, Time: 6ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240715/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//white = -1;
+//black = 1;
+
+vector<int> tiles(201);
+
+int R(int step, int pointer){
+    for(int i = 0; i < step; i++){
+        tiles[++pointer] = 1;
+    }
+
+    return pointer;
+}
+
+int L(int step, int pointer){
+    for(int i = 0; i < step; i++){
+        tiles[pointer--] = -1;
+    }
+
+    return pointer;
+}
+
+int main() {
+    int dir_num, step, pointer = 100;
+    char direction;
+    cin >> dir_num;
+
+    
+
+    for(int i = 0; i < dir_num; i++){
+        cin >> step >> direction;
+        if(direction == 'R'){
+            pointer = R(step, pointer);
+        }
+        else if(direction == 'L'){
+            pointer = L(step, pointer);
+        }
+    }
+    int white = count(tiles.begin(), tiles.end(), -1);
+    int black = count(tiles.begin(), tiles.end(), 1);
+
+    cout << white << ' ' << black;
+
+
+    return 0;
+}
