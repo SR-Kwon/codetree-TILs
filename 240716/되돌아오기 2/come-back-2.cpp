3834c78c266d2604b350b450c400629a13856982
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-16 19:13:20 +0900

    Title: 되돌아오기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240716/되돌아오기 2/come-back-2.cpp
+++ 240716/되돌아오기 2/come-back-2.cpp
@@ -3,49 +3,28 @@ using namespace std;
 
 #define DIR_NUM 4
 
-//E W S N
-int dx[DIR_NUM] = {1,-1,0,0};
-int dy[DIR_NUM] = {0,0,-1,1};
-
-int dir_to_num(char dir){
-    if(dir == 'E') return 0;
-    else if(dir == 'W') return 1;
-    else if(dir == 'S') return 2;
-    else if(dir == 'N') return 3;
-}
-
-char R(char dir){
-    if(dir == 'E') return 'S';
-    else if(dir == 'W') return 'N';
-    else if(dir == 'S') return 'W';
-    else if(dir == 'N') return 'E';
-}
-
-char L(char dir){
-    if(dir == 'E') return 'N';
-    else if(dir == 'W') return 'S';
-    else if(dir == 'S') return 'E';
-    else if(dir == 'N') return 'W';
-}
+//N E S W
+int dx[DIR_NUM] = {0,1,0,-1};
+int dy[DIR_NUM] = {1,0,-1,0};
 
 int main() {
     int x = 0, y = 0, count = 0;
     string line;
     getline(cin, line);
-    char dir = 'N';
+    int dir = 0;
     char cmd;
 
     for(int i = 0; i < line.length(); i++){
         cmd = line[i];
         if(cmd == 'F'){ 
-            x += dx[dir_to_num(dir)];
-            y += dy[dir_to_num(dir)];
+            x += dx[dir];
+            y += dy[dir];
         }
         else if(cmd == 'R'){
-            dir = R(dir);
+            dir = (dir + 1) % 4;
         }
         else if(cmd == 'L'){
-            dir = L(dir);
+            dir = (dir - 1) % 4;
         }
         count++;
         // cout << x << ' ' << y << ' ' << dir << ' ' << cmd <<endl;
