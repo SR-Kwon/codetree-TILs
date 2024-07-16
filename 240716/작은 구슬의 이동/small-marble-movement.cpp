65f020c35e1bd3f28217c7ed8d1156c20c78275b
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-16 18:01:16 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+#define DIR_NUM 4
+#define MAX_NUM 50
+
+//위 아래 오른쪽 왼쪽
+int dx[DIR_NUM] = {0,0,1,-1};
+int dy[DIR_NUM] = {1,-1,0,0};
+
+bool in_range(int x, int y, int n){
+    return (x <= n && 0 <= x && 0 <= y && y <= n);
+}
+
+int dir_to_num(char dir){
+    if(dir == 'U') return 0;
+    else if(dir == 'D') return 1;
+    else if(dir == 'R') return 2;
+    else if(dir == 'L') return 3;
+}
+
+int main() {
+    int n, t;
+    int x, y;
+    char dir;
+    cin >> n >> t;
+    cin >> y >> x >> dir;
+    y--;
+    x--;
+    for(int i = 0; i < t; i++){
+        int nx = x + dx[dir_to_num(dir)];
+        int ny = y + dy[dir_to_num(dir)];
+        if(!in_range(nx, ny, n)){
+            if(dir == 'U') dir = 'D';
+            else if(dir == 'D') dir = 'U';
+            else if(dir == 'R') dir = 'L';
+            else if(dir == 'L') dir = 'R';
+        }
+        else{
+            x = nx;
+            y = ny;
+        }
+    }
+
+    cout << y + 1 << ' ' << x + 1;
+
+    return 0;
+}
