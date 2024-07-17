c63e445adc5777804d24ecbaab4e6eb046271f26
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-17 22:18:12 +0900

    Title: 격자 위의 편안한 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_NUM 100
+#define DIR_NUM 4
+
+int mat[MAX_NUM][MAX_NUM];
+// U D L R
+int dx[DIR_NUM] = {0,0,-1,1};
+int dy[DIR_NUM] = {1,-1,0,0};
+
+bool val_range(int x, int y, int n){
+    return (0 <= x && x < n && 0 <= y && y < n);
+}
+
+
+bool val_safe(int x, int y){
+    int count = 0;
+    for(int i = 0; i < 4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(mat[nx][ny] == 1) count++;
+    }
+    
+    return count >= 3;
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    for(int i = 0; i < M; i++){
+        int x, y;
+        cin >> x >> y;
+        mat[x][y] = 1;
+    }
+
+
+
+    return 0;
+}
