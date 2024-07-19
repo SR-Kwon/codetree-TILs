d8c8b3b4cec7fb3e6fa961d98fe6c559b7c8cb42
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-19 18:30:57 +0900

    Title: 특정 수와 근접한 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, S, all = 0;
+    vector<int> sums;
+
+    cin >> N >> S;
+    int num[N];
+
+    for(int i = 0; i < N; i++){
+        cin >> num[i];
+        all += num[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int t = 0; t < N; t++){
+            sums.push_back(all - num[i] - num[t]);
+        }
+    }
+
+    int min_index, min_dif = 999999;
+    for(int i = 0; i < sums.size(); i++){
+        if(abs(sums[i] - S) < min_dif){
+            min_dif = abs(sums[i] - S);
+            min_index = i;
+
+        }
+    }
+
+    cout << min_dif;
+
+    return 0;
+}
