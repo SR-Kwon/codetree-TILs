33f70189ce571baf4f10f2828d85529c6899fa7d
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-20 20:47:26 +0900

    Title: 개발자의 능력 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/개발자의 능력 3/ability-of-developer-3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+int numbers[6];
+
+
+int GetDiff(int i, int j, int t) {
+    int sum1 = numbers[i] + numbers[j] + numbers[t];
+    int sum2 =  0;
+    for(int k = 0; k < 6; k++)
+        sum2 += numbers[k];
+    sum2 -= sum1;
+    return abs(sum1 - sum2);
+}
+
+
+int main() {
+    for(int i = 0; i < 6; i++)
+        cin >> numbers[i];
+
+    int min_diff = 100;
+    
+    for(int i = 0; i < 6; i++)
+        for(int j = i + 1; j < 6; j++)
+            for(int t = j + 1; t < 6; t++)
+                min_diff = min(min_diff, GetDiff(i, j, t));
+    
+    cout << min_diff;
+    
+    return 0;
+}
