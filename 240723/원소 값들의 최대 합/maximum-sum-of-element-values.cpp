8531b72db015ea9e17a14681f074b1c5f2019aaf
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-23 23:03:00 +0900

    Title: 원소 값들의 최대 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240723/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
+++ 240723/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
@@ -10,18 +10,25 @@ int get_move(int start){
     int current = start;
 
     for(int i = 0; i < m; i++){
-        sum += v[current - 1];
+        //받은 인덱스에 있는 수를 가져옴
         int next = v[current - 1];
-        
-        if (next == current) {
+            //만약 이미 가있다면 break 한다.
+        if(next == current){
             break;
         }
-        
+            //아니면 sum에 더해준다.
+        else{
+            sum += current;
+        }
+        //가야하는곳에 있던 수를 다음에 사용할수 있게 start로 바꿔준다.
         current = next;
+        // //해당 수가 가야하는곳으로 옯겨준다.
+        // v[temp - 1] = temp;
     }
 
     return sum;
 }
+
 
 int main() {
     int n, max_sum = -1;
