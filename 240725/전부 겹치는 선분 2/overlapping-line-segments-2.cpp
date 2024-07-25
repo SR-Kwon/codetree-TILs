a318d74eb076af2ca3a65651297bb3bdfaf5083c
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-25 18:57:05 +0900

    Title: 전부 겹치는 선분 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 240725/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -2,27 +2,31 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
-    int line[101];
-    for(int i=0;i<n;i++){
-        int a,b;
-        cin>>a>>b;
-        for(int i=a;i<=b;i++){
-            line[i]++;
-        }
-    }
+    int x1[100] = {0};
+    int x2[100] = {0};
 
-    for(int i=2;i<=100;i++){
-        if(line[i]==n){
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> x1[i] >> x2[i];
+    
+    for(int i=0; i<n; i++){
+        int arr[101] = {0}; 
+        for(int j=0; j<n; j++){
+            if(i==j) continue; //i번째 선분 제거
+
+            for(int k=x1[j]; k<=x2[j]; k++)
+                arr[k]++;
             
-            cout<<"Yes";
-            return 0;
+            for(int k=0; k<=100; k++){
+                if(arr[k] == n-1){
+                    cout << "Yes";
+                    return 0;
+                }
+            }
         }
     }
-
-    cout<<"No";
+    cout << "No";
 
     return 0;
 }
