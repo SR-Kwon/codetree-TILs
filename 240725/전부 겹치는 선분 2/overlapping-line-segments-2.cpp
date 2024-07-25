b2e964052349c0aaa6a5bf67ad7e13d265ca342b
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-25 18:55:45 +0900

    Title: 전부 겹치는 선분 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240725/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 240725/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -1,46 +1,28 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-    int n, x1, x2;
-    vector<int> lines[n];
-    cin >> n;
-
-    int min, max, count = 0;
-    cin >> min >> max;
-
-    for(int i = 0; i < n-1; i++){
-        cin >> x1 >> x2;
-        //왼쪽
-        if(x1 <= min && x2 < max && x2 >= min){
-            min = x1;
-        }
-        //오른쪽
-        else if(min < x1 && max <= x2 && x1 <= max){
-            max = x2;
-        }
-        //안쪽
-        else if(min <= x1 && x2 <= max){
-            //pass
-        }
-        //바깥
-        else if(min > x1 && x2 > max){
-            min = x1;
-            max = x2;
-        }
-        //안겹침
-        else{
-            count++;
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int line[101];
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        for(int i=a;i<=b;i++){
+            line[i]++;
         }
     }
 
-    if(count == 1 || count == (n-1)){
-        cout << "Yes";
+    for(int i=2;i<=100;i++){
+        if(line[i]==n){
+            
+            cout<<"Yes";
+            return 0;
+        }
     }
-    else{
-        cout << "No";
-    }
+
+    cout<<"No";
 
     return 0;
 }
