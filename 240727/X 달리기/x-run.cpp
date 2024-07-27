b279e8f775494cf3a1b652f6cfa580a556c7f846
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-27 23:07:06 +0900

    Title: X 달리기, Time: 1082ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240727/X 달리기/x-run.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    int current_speed = 1;
+    int current_position = 0;
+    int count = 0;
+
+    while(current_position != x){
+        current_position += current_speed;
+        if(x % 2 == 0){
+            if(current_position < x / 2)
+                current_speed++;
+            else if(current_speed == 1){
+                //pass
+            }
+            else if(current_position >= x / 2)
+                current_speed--;
+        }
+        else{
+            if(current_position < x / 2 + 1)
+                current_speed++;
+            else if(current_speed == 1){
+                //pass
+            }
+            else if(current_position >= x / 2 + 1)
+                current_speed--; 
+        }
+
+        count++;
+    }
+
+    cout << count;
+
+    return 0;
+}
