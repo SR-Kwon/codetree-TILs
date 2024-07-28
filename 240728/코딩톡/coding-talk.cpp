79817960789e88f5e87df360a666220465c3264b
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-28 19:02:38 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/코딩톡/coding-talk.cpp
+++ 240728/코딩톡/coding-talk.cpp
@@ -5,7 +5,6 @@ int main() {
     int n, m, p;
     char his_name[m];
     int his_unseen[m];
-    int stat[n] = {0}; //1읽음 0안읽음
 
     cin >> n >> m >> p;
     
@@ -15,21 +14,26 @@ int main() {
 
     //p부터 읽었는지 확인
     //만약 읽지 않은사람이 0 이면 모두가 읽은거지
-    //뒤에 메세지를 보냈으면 읽은거지 ㅇㅇ
-    for(int i = p - 1; i < m; i++){
-        stat[his_name[i] - 'A'] = 1;
-        // cout << his_unseen[i] << endl;
-
-        if(his_unseen[i] == 0){
-            for(int t = 0; t < n; t++){
-                stat[t] = 1;
-            }
-        }
+    if(his_unseen[p - 1] == 0){
+        return 0;
     }
 
     for(int i = 0; i < n; i++){
-        if(stat[i] == 0)
-            cout << char(i + 'A') << ' ';
+        char person = 'A' + i;
+        bool read = false;
+
+        // 만약 p번 메시지를 읽은 사람 수와 같은 채팅을 기준으로
+        // 한번이라도 채팅을 쳤다면 확실하게 채팅을 읽었습니다.
+        for(int i = 0; i < m; i++){
+            if(his_unseen[i] >= his_unseen[p - 1] && his_name[i] == person){
+                read = true;
+                break;
+            }
+        }
+
+        if(!read){
+            cout << person << ' ';
+        }
     }
 
     return 0;
