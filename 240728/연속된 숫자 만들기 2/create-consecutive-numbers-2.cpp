330875b928b124db5e29f56084170233b306b737
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-28 20:13:28 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240728/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240728/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -20,15 +20,28 @@ int main() {
         else if((pos[2] - pos[1]) == 2){
             pos[0] = pos[2] - 1;
         }
-        // 3. 01, 12 사이 거리중 작은곳에 홀수인경우(뒤에 가깝게)
-        if((pos[1] - pos[0]) >= (pos[2] - pos[1]) && (pos[2] - pos[1]) % 2 == 1){
+        // 3. 01, 12사이의 거리가 1이면 짝수 홀수 판단해서 하자
+        else if((pos[1] - pos[0]) == 1 && (pos[2] - pos[1]) % 2 == 1){
             pos[0] = pos[2] - 1;
         }
-        else if((pos[1] - pos[0]) < (pos[2] - pos[1]) && (pos[1] - pos[0]) % 2 == 1){
+        else if((pos[2] - pos[1]) == 1 && (pos[1] - pos[0]) % 2 == 1){
             pos[2] = pos[1] - 1;
         }
-        // 4. 01, 12 사이 거리중 작은곳이 짝수인경우(앞에 가깝게)
-        if((pos[1] - pos[0]) >= (pos[2] - pos[1]) && (pos[2] - pos[1]) % 2 == 0){
+        else if((pos[1] - pos[0]) == 1 && (pos[2] - pos[1]) % 2 == 0){
+            pos[0] = pos[2] - 1;
+        }
+        else if((pos[2] - pos[1]) == 1 && (pos[1] - pos[0]) % 2 == 0){
+            pos[2] = pos[1] - 1;
+        }
+        // 4. 01, 12 사이 거리중 작은곳에 홀수인경우(뒤에 가깝게)
+        else if((pos[1] - pos[0]) >= (pos[2] - pos[1]) && (pos[2] - pos[1]) % 2 == 1){
+            pos[0] = pos[1] + 1;
+        }
+        else if((pos[1] - pos[0]) < (pos[2] - pos[1]) && (pos[1] - pos[0]) % 2 == 1){
+            pos[2] = pos[0] + 1;
+        }
+        // 5. 01, 12 사이 거리중 작은곳이 짝수인경우(앞에 가깝게)
+        else if((pos[1] - pos[0]) >= (pos[2] - pos[1]) && (pos[2] - pos[1]) % 2 == 0){
             pos[0] = pos[1] + 1;
         }
         else if((pos[1] - pos[0]) < (pos[2] - pos[1]) && (pos[1] - pos[0]) % 2 == 0){
