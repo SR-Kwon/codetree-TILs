f4951fee87324e6d55735aeb7a1f42a53104a1db
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-28 20:31:31 +0900

    Title: 연속된 숫자 만들기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240728/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -7,35 +7,20 @@ int main() {
     cin >> pos[0] >> pos[1] >> pos[2];
     sort(pos, pos + 3);
 
-    while(true){
-        // 1. 맞으면 출력
-        if((pos[1] - pos[0]) == 1 && (pos[2] - pos[1]) == 1){
-            cout << cnt;
-            break;
-        }
-        // 2. 01, 12 사이 거리가 1이면 거기로 나머지 하나 넣기
-        if((pos[1] - pos[0]) == 2){
-            cout << cnt + 1;
-            break;
-        }
-        else if((pos[2] - pos[1]) == 2){
-            cout << cnt + 1;
-            break;
-        }
-        // 3. 01, 12사이의 거리가 1이면 +2 하면 끝
-        else if((pos[1] - pos[0]) == 1 || (pos[2] - pos[1]) == 1){
-            cout << cnt + 2;
-            break;
-        }
-        // 4. 01, 12 사이 거리중 작은곳에 홀수인경우(뒤에 가깝게)
-        else{
-            cout << cnt + 2;
-            break;
-        }
-        sort(pos, pos + 3);
-        // cout << pos[0] << ' ' << pos[1] << ' ' << pos[2] << endl;
-        cnt++;
+    // 1. 맞으면 출력
+    if((pos[1] - pos[0]) == 1 && (pos[2] - pos[1]) == 1){
+        cnt = 0;
     }
+    // 2. 01, 12 사이 거리가 1이면 한번이면 끝나지
+    else if((pos[1] - pos[0]) == 2 || (pos[2] - pos[1]) == 2){
+        cnt = 1;
+    }
+    // 3. 01, 12사이의 거리가 뭐든간에 2번이면 끝
+    else{
+        cnt = 2;
+    }
+
+    cout << cnt;
 
     return 0;
 }
