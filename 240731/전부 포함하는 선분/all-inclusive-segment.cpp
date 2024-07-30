858ef4b581ef049483f247ea833075d725f3cf5a
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-07-31 00:24:41 +0900

    Title: 전부 포함하는 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/전부 포함하는 선분/all-inclusive-segment.cpp
+++ 240731/전부 포함하는 선분/all-inclusive-segment.cpp
@@ -29,3 +29,74 @@ int main() {
     cout << min_len;
     return 0;
 }
+
+/*
+
+해설은 가장 작은x1또는 가장 큰 x2를 가진애를 없애서 구했다
+시간복잡도 O(N)으로 끝낸다.
+
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n;
+int x1[MAX_N], x2[MAX_N];
+int ans = INT_MAX;
+
+int main() {
+    // 입력
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> x1[i] >> x2[i];
+    
+    // 가장 시작점이 작은 선분을 지우거나, 가장 끝점이 큰 선분을 지울 때에만
+    // 전부 포함하는 선분이 줄어들 가능성이 있습니다.
+    // 가장 시작점이 작은 선분을 찾습니다.
+    int skip = 0;
+    for(int i = 0; i < n; i++) {
+        if(x1[skip] > x1[i]) skip = i;
+    }
+    
+    int max_x2 = 0;
+    int min_x1 = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        if(i == skip) continue;
+
+        // 시작점 중 가장 앞에 있는 좌표와 끝점 중 가장 뒤에 있는 점의 좌표를 확인합니다.
+        max_x2 = max(max_x2, x2[i]);
+        min_x1 = min(min_x1, x1[i]);
+    }
+
+    // 조건을 만족하는 선분의 길이는 max_x2 - min_x1입니다.
+    // 정답보다 이 선분의 길이가 작으면 업데이트합니다.
+    ans = min(ans, max_x2 - min_x1);
+
+
+    // 가장 끝점이 큰 선분을 찾습니다.
+    skip = 0;
+    for(int i = 0; i < n; i++) {
+        if(x2[skip] < x2[i]) skip = i;
+    }
+    
+    max_x2 = 0;
+    min_x1 = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        if(i == skip) continue;
+
+        // 시작점 중 가장 앞에 있는 좌표와 끝점 중 가장 뒤에 있는 점의 좌표를 확인합니다.
+        max_x2 = max(max_x2, x2[i]);
+        min_x1 = min(min_x1, x1[i]);
+    }
+
+    // 조건을 만족하는 선분의 길이는 max_x2 - min_x1입니다.
+    // 정답보다 이 선분의 길이가 작으면 업데이트합니다.
+    ans = min(ans, max_x2 - min_x1);
+
+    cout << ans;
+    return 0;
+}
+*/
