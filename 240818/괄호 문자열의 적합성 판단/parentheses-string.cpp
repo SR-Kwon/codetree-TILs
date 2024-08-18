715433e56f726a520f9d31a519485049dec3839f
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-08-18 22:09:13 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240818/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+    string line;
+    stack<char> s;
+    cin >> line;
+
+    for(int i = 0; i < line.length(); i++){
+        if(line[i] == '('){
+            s.push(line[i]);
+        }
+        else if(line[i] == ')'){
+            if(s.top() == '('){
+                s.pop();
+            }
+            else{
+                cout << "No";
+                return 0;
+            }
+        }
+    }
+
+    if(!s.empty()){
+        cout << "No";
+    }
+    else{
+        cout << "Yes";
+    }
+
+    return 0;
+}
