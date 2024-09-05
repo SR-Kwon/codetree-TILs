be1cb95b2a5ceb35d169cb6d6cf2fb954cda83a1
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-09-05 20:43:22 +0900

    Title: 1차원 바람, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240905/1차원 바람/The-1D-wind-blows.cpp
+++ 240905/1차원 바람/The-1D-wind-blows.cpp
@@ -2,109 +2,114 @@
 
 #define MAX_N 100
 #define MAX_M 100
-#define MAX_Q 100
+#define SHIFT_RIGHT 0
+#define SHIFT_LEFT 1
+
 using namespace std;
 
+// 전역 변수 선언:
 int n, m, q;
-int grid[MAX_N][MAX_M];     // 초기 건물 상태 
-int row[MAX_Q];             // 바람에 영향을 받는 행 번호
-char dirs[MAX_Q];           // 바람이 불어오는 방향
 
-void DoWind(int r, char d){ // r행을 d 방향으로 미는 함수
-    if(d == 'L'){   // 오른쪽으로 밀어야 한다면
-        int temp = grid[r][m-1];
-        for(int i=m-1; i>=1; i--)
-            grid[r][i] = grid[r][i-1];
-        grid[r][0] = temp;
+int a[MAX_N + 1][MAX_M + 1];
+
+// row 줄의 원소들을 dir 방향에 따라 한 칸 밀어줍니다.
+// dir이 0인 경우 오른쪽으로
+// dir이 1인 경우 왼쪽으로 밀어야 합니다.
+void Shift(int row, int dir) {
+    // 오른쪽으로 밀어야 하는 경우
+    if(dir == SHIFT_RIGHT) {
+        int temp = a[row][m];
+        for(int col = m; col >= 2; col--)
+            a[row][col] = a[row][col - 1];
+        a[row][1] = temp;
     }
-    
-    else{           // 왼쪽으로 밀어야 한다면
-        int temp = grid[r][0];
-        for(int i=0; i<m-1; i++)
-            grid[r][i] = grid[r][i+1];
-        grid[r][m-1] = temp;
+    // 왼쪽으로 밀어야 하는 경우
+    else {
+        int temp = a[row][1];
+        for(int col = 1; col <= m - 1; col++)
+            a[row][col] = a[row][col + 1];
+        a[row][m] = temp;
     }
 }
 
-bool IsMovable(int s, int e){   // 옮겨서 바람을 불 수 있는지 확인하는 함수
-    // 범위 벗어난 행이면 더이상 이동 불가
-    if(e < 0 || e >= n) return false;
-
-    // 모든 열을 보면서 같은 열에 같은 숫자가 적힌 것이 하나라도 있다면 이동 가능
-    for(int i=0; i<m; i++){
-        if(grid[s][i] == grid[e][i])
+// row1, row2 행에 대해 같은 열에 같은 숫자를 갖는 경우가
+// 있는지를 찾아줍니다.
+bool HasSameNumber(int row1, int row2) {
+    for(int col = 1; col <= m; col++)
+        if(a[row1][col] == a[row2][col])
             return true;
-    }
-    // 같은 숫자를 가진 열이 하나라도 없다면 이동 불가
+    
     return false;
 }
 
-void CheckUpward(int r, char d){    // r행을 d 방향으로 바람 분 이후 위쪽 행을 확인하는 함수
-    int curR = r-1, beforeR = r;
-    char curD = ' ', beforeD = d;
-
-    // 이동 가능할 때까지
-    while(IsMovable(beforeR, curR)){
-        if(beforeD == 'L') curD = 'R';
-        else        curD = 'L';
-        
-        DoWind(curR, curD);
-        beforeR = curR;
-        curR--;
-        beforeD = curD;
-    }
-
+// 주어진 방향으로부터 반대 방향의 값을 반환합니다.
+bool Flip(int dir) {
+    return (dir == SHIFT_LEFT) ? SHIFT_RIGHT : SHIFT_LEFT;
 }
 
-void CheckDownward(int r, char d){  // r행을 d 방향으로 바람 분 이후 아래쪽 행을 확인하는 함수
-    int curR = r+1, beforeR = r;
-    char curD = ' ', beforeD = d;
-    // 이동 가능할 때까지
-    while(IsMovable(beforeR, curR)){
-        if(beforeD == 'L') curD = 'R';
-        else        curD = 'L';
-        
-        DoWind(curR, curD);
-        beforeR = curR;
-        curR++;
-        beforeD = curD;
+// 조건에 맞춰 움직여봅니다.
+// dir이 SHIFT_RIGHT 인 경우 오른쪽으로
+// dir이 SHIFT_LEFT 인 경우 왼쪽으로 밀어야 합니다.
+void Simulate(int start_row, int start_dir) {
+    // Step1
+    // 바람이 처음으로 불어 온 행의 숫자들을 해당 방향으로 밀어줍니다.
+    Shift(start_row, start_dir);
+
+    // 그 이후부터는 반전된 방향에 영향을 받으므로, 방향을 미리 반전 시켜줍니다.
+    start_dir = Flip(start_dir);
+
+    // Step2
+    // 위 방향으로 전파를 계속 시도해봅니다.
+    for(int row = start_row, dir = start_dir; row >= 2; row--) {
+        // 인접한 행끼리 같은 숫자를 가지고 있다면
+        // 위의 행을 한 칸 shift하고
+        // 방향을 반대로 바꿔 계속 전파를 진행합니다.
+        if(HasSameNumber(row, row - 1)) {
+            Shift(row - 1, dir);
+            dir = Flip(dir);
+        }
+        // 같은 숫자가 없다면 전파를 멈춥니다.
+        else
+            break;
     }
-}
 
-void Simulation(){
-    for(int i=0; i<q; i++){
-        int curR = row[i];
-        char curD = dirs[i];
-        // Step 1. curR행을 curD 방향으로 밀기
-        DoWind(curR, curD);
-
-        // Step 2. curR행 위쪽으로 확인
-        CheckUpward(curR, curD);
-        // Step 3. curR행 아래쪽으로 확인
-        CheckDownward(curR, curD);
+    // Step3
+    // 아래 방향으로 전파를 계속 시도해봅니다.
+    for(int row = start_row, dir = start_dir; row <= n - 1; row++) {
+        // 인접한 행끼리 같은 숫자를 가지고 있다면
+        // 아래 행을 한 칸 shift하고
+        // 방향을 반대로 바꿔 계속 전파를 진행합니다.
+        if(HasSameNumber(row, row + 1)) {
+            Shift(row + 1, dir);
+            dir = Flip(dir);
+        }
+        // 같은 숫자가 없다면 전파를 멈춥니다.
+        else
+            break;
     }
 }
 
 int main() {
     // 입력:
     cin >> n >> m >> q;
-    for(int i=0; i<n; i++)
-        for(int j=0; j<m; j++)
-            cin >> grid[i][j];
     
-    for(int i=0; i<q; i++){
-        cin >> row[i] >> dirs[i];
-        row[i]--;
+    for(int row = 1; row <= n; row++)
+        for(int col = 1; col <= m; col++)
+            cin >> a[row][col];
+    
+    while(q--) {
+        int r; char d;
+        cin >> r >> d;
+
+        // 조건에 맞춰 움직여봅니다.
+        Simulate(r, d == 'L' ? SHIFT_RIGHT : SHIFT_LEFT);
     }
 
-    // 바람 부는 시뮬레이션 시작
-    Simulation();
-
     // 출력:
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++)
-            cout << grid[i][j] << ' ';
-        cout << '\n';
+    for(int row = 1; row <= n; row++) {
+        for(int col = 1; col <= m; col++)
+            cout << a[row][col] << " ";
+        cout << endl;
     }
     return 0;
 }
