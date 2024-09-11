e12747c8e1a6cfc72a8fc083d182203b6bdecad4
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-09-11 23:18:26 +0900

    Title: 기울어진 직사각형의 회전, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
+++ 240911/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
@@ -1,98 +1,68 @@
 #include <iostream>
+#include <vector>
+
+#define MAX_N 100
+#define DIR_NUM 4
+#define CCW 0
+#define CW 1
 
 using namespace std;
 
-int arr[105][105], n;
+int n;
+int grid[MAX_N][MAX_N];
+int temp[MAX_N][MAX_N];
 
-void rotate(int r, int c, int m1, int m2, int m3, int m4, int dir){
-    // 회전 시키는 함수
+void Shift(int x, int y, int k, int l, int move_dir) {
+    vector<int> dx, dy, move_nums;
+	
+	if(move_dir == CCW) {
+		dx = {-1, -1, 1, 1};
+		dy = {1, -1, -1, 1};
+		move_nums = {k, l, k, l};
+	}
+	else {
+		dx = {-1, -1, 1, 1};
+		dy = {-1, 1, 1, -1};
+		move_nums = {l, k, l, k};
+	}
+    
+    // Step1. temp 배열에 grid 값을 복사합니다.
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			temp[i][j] = grid[i][j];
 
-    if(dir == 0) {
-        // 반시계 방향 회전
-        int ds[4][2] = {{-1, 1}, {-1, -1}, {1, -1}, {1, 1}};
-        
-        // 1번 방향
-        int temp = arr[r + m1*ds[0][0]][c + m1*ds[0][1]];
-        for(int i = m1; i >= 1; --i) {
-            arr[r + i*ds[0][0]][c + i*ds[0][1]] = arr[r + (i-1)*ds[0][0]][c + (i-1)*ds[0][1]];
+    // Step2. 기울어진 직사각형의 경계를 쭉 따라가면서
+    //        숫자를 한 칸씩 밀었을 때의 결과를
+    //        temp에 저장합니다.
+    for(int d = 0; d < DIR_NUM; d++)
+        for(int q = 0; q < move_nums[d]; q++) {
+            int nx = x + dx[d], ny = y + dy[d];
+            temp[nx][ny] = grid[x][y];
+			x = nx; y = ny;
         }
-        r += m1*ds[0][0], c += m1*ds[0][1];
-        
-        // 2번 방향
-        int temp2 = arr[r + m2 * ds[1][0]][c + m2 * ds[1][1]];
-        for(int i = m2; i >= 2; --i) {
-            arr[r + i*ds[1][0]][c + i*ds[1][1]] = arr[r + (i-1)*ds[1][0]][c +(i-1) * ds[1][1]];
-        }
-        arr[r + ds[1][0]][c + ds[1][1]] = temp;
-        r += m2*ds[1][0], c += m2*ds[1][1];
-
-        // 3번 방향
-        int temp3 = arr[r + m3*ds[2][0]][c + m3*ds[2][1]];
-        for(int i = m3; i >= 2; --i) {
-            arr[r + i*ds[2][0]][c + i*ds[2][1]] = arr[r + (i-1)*ds[2][0]][c +(i-1) * ds[2][1]];
-        }
-        arr[r +ds[2][0]][c+ds[2][1]] = temp2;
-        r += m3 * ds[2][0], c += m3 * ds[2][1];
-        
-        // 4번 방향
-        for(int i = m4; i >= 2; --i) {
-            arr[r + i*ds[3][0]][c + i*ds[3][1]] = arr[r + (i-1)*ds[3][0]][c +(i-1) * ds[3][1]];
-        }
-        arr[r+ds[3][0]][c+ds[3][1]] = temp3;
-    }
-    else {
-        // 시계 방향 회전
-        int ds[4][2] = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
-
-        // 4번 방향
-        int temp = arr[r + m4*ds[0][0]][c + m4*ds[0][1]];
-        for(int i = m4; i >= 1; --i) {
-            arr[r + i*ds[0][0]][c + i*ds[0][1]] = arr[r + (i-1)*ds[0][0]][c + (i-1)*ds[0][1]];
-        }
-        r += m4*ds[0][0], c += m4*ds[0][1];
-
-        // 3번 방향
-        int temp2 = arr[r + m3 * ds[1][0]][c + m3 * ds[1][1]];
-        for(int i = m3; i >= 2; --i) {
-            arr[r + i*ds[1][0]][c + i*ds[1][1]] = arr[r + (i-1)*ds[1][0]][c +(i-1) * ds[1][1]];
-        }
-        arr[r + ds[1][0]][c + ds[1][1]] = temp;
-        r += m3*ds[1][0], c += m3*ds[1][1];
-
-        // 2번 방향
-        int temp3 = arr[r + m2*ds[2][0]][c + m2*ds[2][1]];
-        for(int i = m2; i >= 2; --i) {
-            arr[r + i*ds[2][0]][c + i*ds[2][1]] = arr[r + (i-1)*ds[2][0]][c +(i-1) * ds[2][1]];
-        }
-        arr[r +ds[2][0]][c+ds[2][1]] = temp2;
-        r += m2 * ds[2][0], c += m2 * ds[2][1];
-
-        // 1번 방향
-        for(int i = m1; i >= 2; --i) {
-            arr[r + i*ds[3][0]][c + i*ds[3][1]] = arr[r + (i-1)*ds[3][0]][c +(i-1) * ds[3][1]];
-        }
-        arr[r+ds[3][0]][c+ds[3][1]] = temp3;
-    }
+    
+    // Step3. temp값을 grid에 옮겨줍니다.
+    for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			grid[i][j] = temp[i][j];
 }
 
-int main(void) {
-    // input
-    cin >> n;
-    for(int i = 0; i < n; ++i)
-        for(int j = 0; j < n; ++j)
-            cin >> arr[i][j];
-
-    int r, c, m1, m2, m3, m4, dir;
-    cin >> r >> c >> m1 >> m2 >> m3 >> m4 >> dir;
-    // dir 1 -> 시계방향 dir 0 반시계방향
-
-    r -= 1, c -= 1;
-    rotate(r, c, m1, m2, m3, m4, dir);
-    for(int i = 0; i < n; ++i) {
-        for(int j = 0; j < n; ++j) {
-            cout << arr[i][j] << ' ';
-        }
-        cout << endl;
-    }
-
+int main() {
+	cin >> n;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++) 
+			cin >> grid[i][j];
+	
+	int x, y, m1, m2, m3, m4, d;
+	cin >> x >> y >> m1 >> m2 >> m3 >> m4 >> d;
+    
+    Shift(x - 1, y - 1, m1, m2, d);
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++)
+			cout << grid[i][j] << " ";
+		cout << endl;
+	}
+	return 0;
 }
