f5aa71eddd66ea86ad5b012403f07da414eb2131
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-09-13 18:18:21 +0900

    Title: 1차원 폭발 게임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240913/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -1,104 +1,63 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<list>
 
-using namespace std;
+int N, M;
+int bombs[201];
+int tmp[201];
 
-int n, m;
-int arr[102];
-int temp[102];
-vector<int> answer;
-vector<int> bombList;
-int count;
-
-void delBomb() {
-    
-    int idx = 1;
-    for(int i = 0; i < bombList.size(); i++) {
-        int del = bombList[i];
-        for(int j = 1; j <= n; j++) {
-            if(arr[j] != del) {
-                temp[idx] = arr[j];
-                idx = idx + 1;
-            }
-        }
-    }
-
-    for(int i = 0; i < bombList.size(); i++) {
-        bombList.pop_back();
-    }
-
-    for(int i = 1; i <= idx; i++) {
-        arr[i] = temp[i];
-    }
-
+bool Boom()
+{
+	if (N == 0) return false;
+	bool isBoom = false;
+	int prev = bombs[0];
+	int cnt = 1;
+	int tmpN = N;
+	for (int i = 1; i < N; i++)
+	{
+		if (prev == bombs[i]) cnt++;
+		else
+		{
+			if (cnt >= M)
+			{
+				tmpN -= cnt;
+				isBoom = true;
+				for (int j = 1; j <= cnt; j++) bombs[i - j] = 0;
+				
+			}
+			cnt = 1;
+		}
+		prev = bombs[i];
+	}
+	if (cnt >= M)
+	{
+		isBoom = true;
+		for (int j = 1; j <= cnt; j++) bombs[N - j] = 0;
+		tmpN -= cnt;
+	}
+	int tmpIdx = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (bombs[i] == 0) continue;
+		tmp[tmpIdx++] = bombs[i];
+	}
+	
+	for (int i = 0; i < N; i++) bombs[i] = 0;
+	for (int i = 0; i < tmpIdx; i++) bombs[i] = tmp[i];
+	N = tmpN;
+	return isBoom;
 }
 
-void checkBomb() {
-    int cnt = 1;
-    for(int i = 1; i < n; i++) {
-        if(arr[i] == arr[i + 1]) {
-            cnt++;
-        }
-        else {
-            cnt = 1;
-        }
+int main(int argc, char** argv)
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL); std::cout.tie(NULL);
 
-        if(cnt >= m) {
-            bool flag = true;
-            for(int j = 0; j < bombList.size(); j++) {
-                if(bombList[j] == arr[i]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag != false) {
-                bombList.push_back(arr[i]);    
-            }
-        }
-    }
-    for(int i = 0; i < n - bombList.size(); i++) {
-        cout << bombList[i] << ' ';
-    }
-    count = bombList.size();
-    //cout << count;
-}
+	std::cin >> N >> M;
+	for (int i = 0; i < N; i++) std::cin >> bombs[i];
+	while (Boom());
 
-void initBomb() {
-    for(int i = 0; i < 102; i++) {
-        temp[i] = 0;
-    }
-}
+	std::cout << N << '\n';
+	for (int i = 0; i < N; i++) std::cout << bombs[i] << '\n';
 
-void print() {
-    for(int i = 1; i <= n; i++) {
-        cout << arr[i] << ' ';
-    }
-    cout << '\n';
-}
-
-int main() {
-    // 여기에 코드를 작성해주세요.
-    cin >> n >> m;
-
-    for(int i = 1; i <= n; i++) {
-        cin >> arr[i];
-    }
-
-   // while(1) {
-        checkBomb();
-        cout << "checkBomob after-------------\n";
-        print();
-     //   if(bombList.empty() == true) {
-         //   break;
-       // }
-        delBomb();
-        //initBomb();
-
-        cout << "delBomob after-------------\n";
-       print();
-  //  }
-    cout << "---------------last answer \n";
-    print();
-
-    return 0;
+	return EXIT_SUCCESS;
 }
