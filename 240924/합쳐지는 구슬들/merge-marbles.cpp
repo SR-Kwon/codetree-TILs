ee8a76a0c26b831d4942dc115bbb5ffc982822bb
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-09-24 20:27:36 +0900

    Title: 합쳐지는 구슬들, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/합쳐지는 구슬들/merge-marbles.cpp
+++ 240924/합쳐지는 구슬들/merge-marbles.cpp
@@ -1,118 +1,131 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <tuple>
+
+#define MAX_N 50
+#define ASCII_NUM 128
+#define DIR_NUM 4
+#define EMPTY make_tuple(0, 0, 0)
 
 using namespace std;
 
 int n, m, t;
-int arr[51][51];
-vector<vector<int> > marble;
+tuple<int, int, int> grid[MAX_N][MAX_N];
+tuple<int, int, int> next_grid[MAX_N][MAX_N];
 
-bool InRange(int x, int y){
-    return x>=0 && x<n && y>=0 && y<n;
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-bool cmp(vector<int> &v1, vector<int> &v2){
-    return v1[0]<v2[0];
-}
-
-int DirConvertToInt(char dir){
-    if(dir=='L')return 0;
-    if(dir=='U')return 1;
-    if(dir=='R')return 2;
-    if(dir=='D')return 3;
-    return -1;
-}
-
-void MoveMarble(){
-    // L(0,-1) U(-1,0) R(0,1) D(1,0)
-    int dx[4]={0, -1, 0, 1};
-    int dy[4]={-1, 0, 1, 0};
-
-    for(int i=0; i<marble.size(); ++i){
-        int dir=marble[i][3];
-        int nx=marble[i][1]+dx[dir];
-        int ny=marble[i][2]+dy[dir];
-        // 범위를 벗어나면 방향만 바꾸기
-        if(!InRange(nx, ny)){
-            dir=(dir+2)%4;
-            marble[i][3]=dir;
-            continue;
-        }
-        // 이동
-        arr[marble[i][1]][marble[i][2]]--;
-        marble[i][1]=nx;
-        marble[i][2]=ny;
-        arr[nx][ny]++;
+tuple<int, int, int> NextPos(int x, int y, int move_dir) {
+	int dx[DIR_NUM] = {-1, 0, 0, 1};
+	int dy[DIR_NUM] = {0, 1, -1, 0};
+    
+    int nx = x + dx[move_dir], ny = y + dy[move_dir];
+    if(!InRange(nx, ny))
+        move_dir = 3 - move_dir;
+    else {
+        x = nx; y = ny;
     }
+    
+    return make_tuple(x, y, move_dir);
 }
 
-//이 구슬들이 합쳐지게 되면 하나의 구슬이 만들어지게 되고,
-//[무게] 해당 위치에 모인 모든 구슬의 합
-//[방향] 합쳐진 구슬들 중 가장 큰 번호가 매겨져있는 구슬의 방향
-//[번호] 충돌이 일어난 구슬들 중 가장 큰 번호
-void MergeMarble(int x, int y){
-    int weight=0, maxNum=0, maxDir=0;
-
-    //(x,y)에 있는 구슬 전부 삭제
-    for(int i=0; i<marble.size(); ++i){
-        if(marble[i][1]==x && marble[i][2]==y){
-            if (marble[i][0] > maxNum) {
-                maxNum = marble[i][0];
-                maxDir = marble[i][3];
-            }
-            weight+=marble[i][4];
-            marble[i][0]=3000; // 1<=m<=n*n 범위 밖의 값 설정
-        }
-    }
-
-    // 합쳐진 구슬 추가
-    marble.push_back({maxNum, x, y, maxDir, weight});
-    m=m-arr[x][y]+1;
-    arr[x][y]=1;
-
-    sort(marble.begin(), marble.end(), cmp);
-
-    while(marble.size()>m){
-        marble.pop_back();
-    }
-
+// (x, y) 위치에 새로운 구슬이 들어왔을 때 갱신을 진행합니다.
+void Update(int x, int y, tuple<int, int, int> new_marble) {
+    // 기존 구슬 정보입니다.
+    int num, weight, move_dir;
+    tie(num, weight, move_dir) = next_grid[x][y];
+    
+    // 새롭게 들어온 구슬 정보입니다.
+    int new_num, new_weight, new_dir;
+    tie(new_num, new_weight, new_dir) = new_marble;
+    
+    // 새로 들어온 구슬이 더 우선순위가 높다면
+    // 번호와 방향은 새로운 구슬을 따르게 됩니다.
+    if(new_num > num) 
+        next_grid[x][y] = make_tuple(new_num, weight + new_weight, new_dir);
+    // 기존 구슬의 우선순위가 더 높다면
+    // 무게만 더해집니다.
+    else
+        next_grid[x][y] = make_tuple(num, weight + new_weight, move_dir);
 }
 
-void Progress(){
-    while(t--){
-        // 1.구슬 이동
-        MoveMarble();
+void Move(int x, int y) {
+    int num, weight, move_dir;
+    tie(num, weight, move_dir) = grid[x][y];
+    
+    // Step1. 현재 구슬의 다음 위치와 방향을 구합니다.
+    int nx, ny, next_dir; 
+    tie(nx, ny, next_dir) = NextPos(x, y, move_dir);
+    
+    // Step2. 구슬을 옮겨줍니다.
+    Update(nx, ny, make_tuple(num, weight, next_dir));
+}
 
-        // 2. 한 칸에 여러 개의 구슬이 있는지 체크
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<n; ++j){
-                // 3. 구슬이 2개 이상인 칸은 구슬 합치기
-                if(arr[i][j]>=2) MergeMarble(i, j);
-            }
-        }
-    }
+void Simulate() {
+    // Step1. next_grid를 초기화합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            next_grid[i][j] = EMPTY;
+    
+    // Step2. 각 구슬들을 한 칸씩 움직여줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(grid[i][j] != EMPTY)
+                Move(i, j);
+
+    // Step3. next_grid 값을 grid로 옮겨줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            grid[i][j] = next_grid[i][j];
+}
+
+int GetMarbleNum() {
+    int cnt = 0;
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+            if(grid[i][j] != EMPTY)
+                cnt++;
+    
+    return cnt;
+}
+
+int GetMaxWeight() {
+    int max_weight = 0;
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+            if(grid[i][j] != EMPTY) {
+			    int weight;
+			    tie(ignore, weight, ignore) = grid[i][j];
+			    max_weight = max(max_weight, weight);
+		    }
+
+    return max_weight;
 }
 
 int main() {
-    // input
-    cin >> n >> m >> t;
-    for(int i=0; i<m; ++i){
-        int r, c, w;
-        char d;
-        cin >> r >> c >> d >> w;
-        marble.push_back({i, r-1, c-1, DirConvertToInt(d), w});
-        arr[r-1][c-1]=1;
-    }
+	cin >> n >> m >> t;
+	
+	int dir_mapper[ASCII_NUM];
+	dir_mapper['U'] = 0;
+	dir_mapper['R'] = 1;
+	dir_mapper['L'] = 2;
+	dir_mapper['D'] = 3;
+	
+	for(int i = 0; i < m; i++) {
+		int r, c, w; char d;
+		cin >> r >> c >> d >> w;
+		grid[r - 1][c - 1] = make_tuple(i + 1, w, dir_mapper[d]);
+	}
+	
+    // t초에 걸쳐 시뮬레이션을 진행합니다.
+	while(t--)
+		Simulate();
+	
+    int marble_num = GetMarbleNum();
+    int max_weight = GetMaxWeight();
     
-    // solution
-    Progress();
-
-    // output
-    int maxWeight=0;
-    for(int i=0; i<marble.size(); ++i){
-        maxWeight=max(maxWeight, marble[i][4]);
-    }
-    cout << marble.size() << " " << maxWeight <<'\n';
-    return 0;
+	cout << marble_num << " " << max_weight;
+	return 0;
 }
