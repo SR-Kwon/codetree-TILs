bdf189802ba93231441a59f340a977f2de71f756
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-10-03 22:24:29 +0900

    Title: 핀볼게임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241003/핀볼게임/pinball-game.cpp
+++ 241003/핀볼게임/pinball-game.cpp
@@ -1,75 +1,55 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
-#include <list>
-#include <unordered_set>
+#include <algorithm>
+
+#define MAX_N 100
+#define DIR_NUM 4
 
 using namespace std;
 
 int n;
-int ans=0;
-int dy[4]={-1,0,1,0};
-int dx[4]={0,1,0,-1};
-vector<vector<int>>map;
-int range(int y,int x){
-    if(y<1||y>n) return 0;
-    if(x<1||x>n) return 0;
-    return 1;
-}
-int t;
-int changedir(int d,int mirror){
-    if(mirror==1){
-        return (5-d)%4;
-    }
-    else if(mirror==2){
-        return 3-d;
-    }
+int grid[MAX_N][MAX_N];
+
+bool InRange(int x, int y) {
+	return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void dfs(int y,int x,int d){
-    t++;
-    int ny=y+dy[d];
-    int nx=x+dx[d];
-    if(!range(ny,nx))return;
-    if(map[ny][nx]==0){
-        dfs(ny,nx,d);
-    }
-    else{
-        int ndir=changedir(d,map[ny][nx]);
-        dfs(ny,nx,ndir);
-    }
+int Calc(int x, int y, int move_dir) {	
+	int dx[DIR_NUM] = {-1, 1, 0, 0};
+	int dy[DIR_NUM] = {0, 0, -1, 1};
+	// 1번 블럭에서는 방향이 다음과 같이 변합니다 : 0<->3 1<->2
+	// 2번 블럭에서는 방향이 다음과 같이 변합니다 : 0<->2 1<->3
+	
+	int elapsed_time = 1;
+	
+	while(InRange(x, y)) {
+		if(grid[x][y] == 1)
+			move_dir = 3 - move_dir;
+		else if(grid[x][y] == 2)
+			move_dir = (move_dir < 2) ? (move_dir + 2) : (move_dir - 2);
+		x += dx[move_dir]; y += dy[move_dir];
+		elapsed_time++;
+	}
+	return elapsed_time;
 }
-
-void sol(int y,int x,int d){
-    t=0;
-    dfs(y,x,d);
-    ans=max(ans,t);
-}
-
 
 int main() {
-
-   
-    cin>>n;
-    map=vector<vector<int>>(n+2,vector<int>(n+2));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cin>>map[i][j];
-        }
-    }
-
-    for(int i=1;i<=n;i++){
-        sol(i,0,1);
-        sol(i,n+1,3);
-        sol(0,i,2);
-        sol(n+1,i,0);
-    }
-    cout<<ans;
-
-
-    
-
-    
-    
-
+	cin >> n;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			cin >> grid[i][j];
+	
+    // 각각의 상하좌우 방향에 대해
+    // 가능한 모든 위치에서 걸리는 시간을 계산한 후,
+    // 그 중 최댓값을 구합니다.
+	int ans = 0;
+	for(int i = 0; i < n; i++) {
+		ans = max(ans, Calc(n - 1, i, 0));
+		ans = max(ans, Calc(0, i, 1));
+		ans = max(ans, Calc(i, n - 1, 2));
+		ans = max(ans, Calc(i, 0, 3));
+	}
+	
+	cout << ans;
+	return 0;
 }
