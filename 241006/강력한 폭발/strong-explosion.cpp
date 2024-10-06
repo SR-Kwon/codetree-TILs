31861c2b5e66b143edcdd291762a6a241cdb1b33
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-10-06 13:33:26 +0900

    Title: 강력한 폭발, Time: 39ms, Memory: 0MB, Status: Passed - CodeTree

--- 241006/강력한 폭발/strong-explosion.cpp
+++ 241006/강력한 폭발/strong-explosion.cpp
@@ -1,101 +1,97 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <tuple>
+
+#define MAX_N 20
+#define BOMB_TYPE_NUM 3
+
 using namespace std;
 
-vector<int> bomb_type;
-int arr[21][21];
+int n;
+int bomb_type[MAX_N][MAX_N];
+bool bombed[MAX_N][MAX_N];
 
-int n; // arr 크기
-int num_of_bomb; // arr에 설치된 폭탄의 갯수
-int answer;
+int ans;
 
-// 만들어진 bomb_type을 바탕으로 폭발구역의 갯수를 반환
-int Check_Exploding_zones(){
-    // bomb_type ={1, 2, 3, 1} 이라면
-    // 역순(1, 3, 2, 1)으로 배치하면서 pop_back하는 형식으로 구현(어차피 모든 조합이 들어있을 것이기 때문에 뒤집어도 상관없음)
-    vector<int> v = bomb_type;
-    int exploding_map[21][21] ={0, };
+vector<pair<int, int> > bomb_pos;
 
-    // arr를 돌기 위한 이중 포문
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(arr[i][j]==0) continue;
-            // 폭탄을 설치할 위치라면 설치할 폭탄의 종류를 조합에서 꺼낸다.
-            int exploding_type = v.back();
-            v.pop_back();
-            
-            if(exploding_type==1){
-                for(int k = i-2; k<=i+2; k++){
-                    if(k<0 || k>=n) continue;
-                    exploding_map[k][j]=1;
-                }
-            }
-            else if(exploding_type==2){
-                exploding_map[i][j]=1;
-                if(i-1>=0) exploding_map[i-1][j]=1; //상
-                if(i+1<n) exploding_map[i+1][j]=1; // 하
-                if(j-1>=0) exploding_map[i][j-1]=1; // 좌
-                if(j+1<n) exploding_map[i][j+1]=1; // 우
-            }
-            else{
-                exploding_map[i][j]=1;
-                if(i-1>=0){
-                    if(j-1>=0) exploding_map[i-1][j-1]=1; //좌상
-                    if(j+1<n) exploding_map[i-1][j+1]=1; //우상
-                }
-                if(i+1<n){
-                    if(j-1>=0) exploding_map[i+1][j-1]=1; //좌하
-                    if(j+1<n) exploding_map[i+1][j+1]=1; //우하
-                }
-            }
-            
-        }
-    }
-
-    int num_of_exploding=0;
-    // exploding_map을 돌면서 폭발 구역의 갯수를 구함
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            //cout << exploding_map[i][j]<<' ';
-            if(exploding_map[i][j]==1) num_of_exploding++;
-        }
-        //cout <<'\n';
-    }
-    return num_of_exploding;
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-// 폭탄타입의 조합을 구하는 함수
-void Make_Bomb_Combination(int bomb){
-    // 종료 조건
-    if(bomb>=num_of_bomb){
-        //cout << "bomb_type: ";
-        //for (auto e : bomb_type){
-        //    cout << e << " ";
-        //}
-        //cout <<'\n';
-        int exploding = Check_Exploding_zones();
-        if(answer<exploding) answer=exploding;
-        return;
+void Bomb(int x, int y, int b_type) {
+    // 폭탄 종류마다 터질 위치를 미리 정의합니다.
+    pair<int, int> bomb_shapes[BOMB_TYPE_NUM + 1][5] = {
+        {},
+        { {-2, 0}, {-1, 0}, {0, 0},  {1, 0}, {2, 0}},
+        { {-1, 0},  {1, 0}, {0, 0}, {0, -1}, {0, 1}},
+        {{-1, -1}, {-1, 1}, {0, 0}, {1, -1}, {1, 1}}
+    };
+    
+    // 격자 내 칸에 대해서만 영역을 표시합니다.
+    for(int i = 0; i < 5; i++) {
+        int dx, dy;
+        tie(dx, dy) = bomb_shapes[b_type][i];
+        
+        int nx = x + dx, ny = y + dy;
+        if(InRange(nx, ny))
+            bombed[nx][ny] = true;
     }
-    // 재귀 호출
-    for(int i=1; i<=3; i++){
-        bomb_type.push_back(i);
-        Make_Bomb_Combination(bomb+1);
-        bomb_type.pop_back();
-    }
+}
+
+int Calc() {
+    // Step1. 폭탄이 터진 위치를 표시하는 배열을
+    // 초기화합니다.
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			bombed[i][j] = false;
+	
+    // Step2. 각 폭탄의 타입에 따라 
+    // 초토화 되는 영역을 표시합니다.
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+            if(bomb_type[i][j])
+                Bomb(i, j, bomb_type[i][j]);
+	
+    // Step3. 초토화된 영역의 수를 구합니다.
+	int cnt = 0;
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+            if(bombed[i][j])
+                cnt++;
+    
+	return cnt;
+}
+
+void FindMaxArea(int cnt) {
+	if(cnt == (int) bomb_pos.size()) {
+		ans = max(ans, Calc());
+		return;
+	}
+	for(int i = 1; i <= 3; i++) {
+        int x, y;
+        tie(x, y) = bomb_pos[cnt];
+        
+		bomb_type[x][y] = i;
+		FindMaxArea(cnt + 1);
+		bomb_type[x][y] = 0;
+	}
 }
 
 int main() {
-    cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin >> arr[i][j];
-            if(arr[i][j]==1) num_of_bomb++;
-        }
-    }
-    //cout << num_of_bomb <<'\n';
-    
-    Make_Bomb_Combination(0);
-    cout << answer;
-    return 0;
+	cin >> n;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++) {
+            int bomb_place;
+			cin >> bomb_place;
+			if(bomb_place)
+				bomb_pos.push_back(make_pair(i, j));
+		}
+	
+	FindMaxArea(0);
+	
+	cout << ans;
+	return 0;
 }
