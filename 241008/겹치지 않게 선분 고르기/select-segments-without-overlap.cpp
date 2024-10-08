93e62d0bada5851f22a2415c9740f66b7fdc1f2b
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-10-08 23:01:31 +0900

    Title: 겹치지 않게 선분 고르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241008/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 241008/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,68 +1,3 @@
-/*
- * 2024.1.4
- * 1. 잦은 실수가 많아서 푸는데 오래 걸림
- * 2. 조합을 백트래킹으로 처음 풀어봐서 풀이를 만들지 못함
-*/
-#include <iostream>
-#include <vector>
-#include <tuple>
-#include <algorithm>
-#define MAX_N 15
-using namespace std;
-
-int n;
-pair<int, int> lines[MAX_N]; 
-vector<int> selected_idx;
-int ans;
-
-bool isOverlapped(){
-    for(int i=0; i<(int)selected_idx.size(); i++){
-        for(int j=0; j<(int)selected_idx.size(); j++){
-            if(i==j) continue;
-            int x1, x2, x3, x4;
-            tie(x1,x2) = lines[selected_idx[i]];
-            tie(x3,x4) = lines[selected_idx[j]];
-            if((x1 <= x3 && x3 <= x2) || (x3 <= x1 && x1 <= x4)){
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-void SelectLine(int num){   // num번째 선분 선택을 결정하는 함수
-    if(num == n){
-        if(!isOverlapped()){
-            // 만약 선분 조합이 겹치지 않으면
-            int cur = (int)selected_idx.size();
-            ans = max(ans, cur);
-        }
-        return;
-    }
-
-    selected_idx.push_back(num);
-    SelectLine(num+1);
-    selected_idx.pop_back();
-
-    SelectLine(num+1);
-}
-
-int main(){
-    // 입력:
-    cin >> n;
-    for(int i=0; i<n; i++){
-        cin >> lines[i].first >> lines[i].second;
-    }
-
-    SelectLine(0);  // 0번째 선분을 확인하러 호출
-    cout << ans;
-    return 0;
-}
-
-/*
- * 해설 풀이
-
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -127,4 +62,3 @@ int main(){
     cout << ans;
     return 0;
 }
-*/
