1f733383268de7c826c136146cb50599fb3d77e1
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-10-09 20:17:58 +0900

    Title: 2명의 도둑, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241009/2명의 도둑/two-thieves.cpp
+++ 241009/2명의 도둑/two-thieves.cpp
@@ -1,80 +1,73 @@
 #include <iostream>
+#include <algorithm>
+
+#define MAX_N 10
+#define MAX_M 5
+
 using namespace std;
 
-int n, m, c;
-int board[15][15];
-/* dp[i][j]는 
- * board[i][j], board[i + 1][j], ..., board[i + m - 1][j] 중
- * 도둑이 가져갈 수 있는 최대 가치를 저장
- */
-int dp[15][15];
+int n,m,c;
+int weight[MAX_N][MAX_N];
 
-void Simulate(const int row, const int start_col, 
-    const int cur_col, const int sum, const int value_sum, int& max_val) 
-{
-    if (cur_col == start_col + m) {
-        if (max_val < value_sum) {
-            max_val = value_sum;
-        }
+int a[MAX_M];
+
+int ans =0 ;
+int max_val;
+
+void FindMaxSum(int curr_idx, int curr_weight, int curr_val){
+    if(curr_idx == m){
+        if(curr_weight <= c)
+            max_val = max(max_val, curr_val);
         return;
     }
 
-    for (int col{ cur_col }; col < start_col + m; col++) {
-        int new_sum = sum + col;
-        if (c < new_sum) continue;
-        Simulate(row, start_col, col + 1, new_sum, value_sum + col * col, max_val);
-    }
+    FindMaxSum(curr_idx+1, curr_weight, curr_val);
+
+    FindMaxSum(curr_idx+1, curr_weight+a[curr_idx], curr_val+a[curr_idx]*a[curr_idx]);
 }
 
-int FindMaxValue(const int row, const int col) {
-    int max_val{ 0 };
-    Simulate(row, col, col, 0, 0, max_val);
 
+int FindMax(int sx, int sy){
+    for(int i=sy; i<=sy + m -1; i++)
+        a[i-sy] = weight[sx][i];
+    
+    max_val = 0;
+    FindMaxSum(0,0,0);
     return max_val;
 }
 
-void CalculateDp() {
-    for (int i{ 0 }; i < n; i++) {
-        for (int j{ 0 }; j + m - 1 < n; j++) {
-            dp[i][j] = FindMaxValue(i, j);
-        }
-    }
+bool Intersect(int a, int b, int c, int d){
+
+    return !(b<c || d<a);
 }
 
-int FindBestComb() {
-    int max_sum{ 0 };
-    for (int i{ 0 }; i < n; i++) {
-        for (int j{ 0 }; j + m - 1 < n; j++) {
-            int sum = dp[i][j];
-            for(int k{ i }; k < n; k++) {
-                for (int l{ 0 }; l + m - 1 < n; l++) {
-                    if (k == i && l <= j) continue;
-                    sum += dp[k][l];
-                    if (max_sum < sum) max_sum = sum;
-                }
-            }
-        }
-    }
+bool Possible(int sx1, int sy1, int sx2, int sy2){
+    if(sy1 + m -1 >= n || sy2 + m -1  >= n)
+        return false;
+    
+    if(sx1 != sx2)
+        return true;
 
-    return max_sum;
+    if(Intersect(sy1, sy1 + m-1, sy2, sy2+m-1))
+        return false;
+    return true;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
+int main(){
     cin >> n >> m >> c;
 
-    for (int i{ 0 }; i < n; i++) {
-        for (int j{ 0 }; j < n; j++) {
-            cin >> board[i][j];
-        }
-    }
-
-    CalculateDp();
-    int ans = FindBestComb();
-
+    for(int i=0; i<n ;i++ )
+        for(int j=0; j<n; j++)
+            cin >> weight[i][j];
+    
+    for(int sx1 = 0; sx1 < n; sx1++)
+        for(int sy1 = 0; sy1< n; sy1++)
+            for(int sx2 = 0; sx2 <n; sx2++)
+                for(int sy2=0; sy2 < n; sy2++){
+                    if(Possible(sx1, sy1, sx2, sy2))
+                        ans = max(ans, FindMax(sx1, sy1)+ FindMax(sx2,sy2));
+                }
+    
     cout << ans;
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
