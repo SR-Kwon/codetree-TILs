323c94712c07e7fa31d1a444b2ec586edee4c7df
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-10-10 20:19:53 +0900

    Title: 사다리 타기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/사다리 타기/ladder-game.cpp
+++ 241010/사다리 타기/ladder-game.cpp
@@ -1,95 +1,74 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
 #include <algorithm>
+#include <vector>
+#include <climits>
 
-#define MAX_M 15
 #define MAX_N 11
+
 using namespace std;
 
 int n, m;
-int result[MAX_N];
-pair<int, int> lines[MAX_M];    // 가로줄의 상태
+
+vector<pair<int, int> > lines;
 vector<pair<int, int> > selected_lines;
-int ans = MAX_N;
-void FindResult(){
-    // 1. Step 1. lines (a,b)를 b 순으로 오름차순 정렬하기
-    sort(lines, lines+m, [](const pair<int, int>& a, const pair<int, int>& b){
-        return a.second < b.second;
-    });
-    // 2. 0번부터 끝까지 lines을 보면서 결과 result에 저장하기
-    for(int i=0; i<n; i++){
-        result[i] = i;
-    }
-    for(int i=0; i<m; i++){
-        int a = lines[i].first;
-        int tmp = result[a];
-        result[a] = result[a+1];
-        result[a+1] = tmp;
-    }
+
+int ans = INT_MAX;
+
+// 처음 상황과, 선택한 가로줄만 사용했을 때의
+// 상황을 시뮬레이션하여
+// 둘의 결과가 같은지 확인합니다.
+bool Possible() {
+    // Step1. 시작 숫자를 셋팅합니다.
+	int num1[MAX_N], num2[MAX_N];
+	for(int i = 0; i < n; i++)
+		num1[i] = num2[i] = i;
+	
+    // Step2. 위에서부터 순서대로 적혀있는 
+    // 가로줄에 대해 양쪽 번호에 해당하는 숫자를 바꿔줍니다. 
+	for(int i = 0; i < (int) lines.size(); i++) {
+		int idx = lines[i].second;
+		swap(num1[idx], num1[idx + 1]);
+	}
+	for(int i = 0; i < (int) selected_lines.size(); i++) {
+		int idx = selected_lines[i].second;
+		swap(num2[idx], num2[idx + 1]);
+	}
+	
+    // Step3. 두 상황의 결과가 동일한지 확인합니다.
+	for(int i = 0; i < n; i++)
+		if(num1[i] != num2[i])
+			return false;
+
+	return true;
 }
 
-bool IsSame(){
-    // Step 1. selected_lines를 두번째 원소 기준으로 오름차순 정렬하기
-    sort(selected_lines.begin(), selected_lines.end(), [](const pair<int, int>& a, const pair<int, int>&b){
-            return a.second < b.second;
-        });
-
-    // Step 2. 0번부터 끝까지 selected_lines을 보면서 결과 저장하기
-    int curResult[n];
-    for(int i=0; i<n; i++)
-        curResult[i] = i;
-    
-    for(int i=0; i<(int)selected_lines.size(); i++){
-        int a = selected_lines[i].first;
-        // a와 a+1번째 바꾸기
-        int tmp = curResult[a];
-        curResult[a] = curResult[a+1];
-        curResult[a+1] = tmp;
-    }
-
-    // Step 3. result와 curResult가 같은지 확인하기
-    for(int i=0; i<n; i++){
-        if(result[i] != curResult[i])
-            return false;
-    }
-    /*
-    */
-    return true;
-
-}
-
-void FindMinLines(int cnt, int total){     // cnt개를 선택하러 가는 함수
-    if((int)selected_lines.size() == total){
-        // 아진짜 너무 피곤하고 졸리다................................................................................................
-        if(IsSame()){
-           ans = min(ans, (int)selected_lines.size());
-        }
-        return;
-    }
-
-    for(int i=cnt; i < m; i++){
-        selected_lines.push_back(lines[i]);
-        FindMinLines(i+1, total);
-        selected_lines.pop_back();
-    }
-
+void FindMinLines(int cnt) {
+	if(cnt == m) {
+		if(Possible())
+			ans = min(ans, (int) selected_lines.size());
+		return;
+	}
+	
+	selected_lines.push_back(lines[cnt]);
+	FindMinLines(cnt + 1);
+	selected_lines.pop_back();
+	
+	FindMinLines(cnt + 1);
 }
 
 int main() {
-    cin >> n >> m;      // n: 세로줄 개수, m: 가로줄 개수
-    for(int i=0; i<m; i++){
-        int a, b;
-        cin >> a >> b;
-        lines[i] = make_pair(a-1,b-1);
-    }
+	cin >> n >> m;
+	
+	for(int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		lines.push_back(make_pair(b, a - 1));
+	}
+	
+	sort(lines.begin(), lines.end());
 
-    FindResult();
-    for(int i=0; i<=m; i++){
-        // 1개부터 m개까지 선분 골라보면서 확인하기
-        FindMinLines(0, i);
-    }
-
-    cout << ans;
-    return 0;
+	FindMinLines(0);
+	
+	cout << ans;
+	return 0;
 }
