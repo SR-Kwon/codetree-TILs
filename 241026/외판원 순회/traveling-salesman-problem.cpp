80b7ab3cff8c282da8ef7bbe40b3f4abef532930
SungRok Kwon <125136338+SR-Kwon@users.noreply.github.com>
2024-10-26 22:24:29 +0900

    Title: 외판원 순회, Time: 40ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/외판원 순회/traveling-salesman-problem.cpp
+++ 241026/외판원 순회/traveling-salesman-problem.cpp
@@ -1,66 +1,75 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <climits>
+#include <algorithm>
+
+#define MAX_N 10
+
 using namespace std;
 
-int a;
-int arr[10][10];
+// 변수 선언
+int n;
+int cost[MAX_N][MAX_N];
+bool visited[MAX_N];
+vector<int> picked;
+
 int ans = INT_MAX;
-bool visited[10001];
-vector<int> v;
 
-int calc(){
-    int cost = 0;
-    v.push_back(0);
-    // for(int i = 0; i < v.size(); i++){
-    //     cout << v[i] << ' ';
-    // }
-    
-    for(int i = 1; i < v.size(); i++){
-        if(arr[v[i - 1]][v[i]] == 0){
-            v.pop_back();
-            return INT_MAX;
+// 지금까지 방문한 지점의 수를 cnt라 했을 때
+// 계속 탐색을 이어서 진행합니다.
+void FindMin(int cnt) {
+    // 모든 지점를 방문했을 때
+    // 가능한 지금까지의 비용 합 중
+    // 최솟값을 갱신합니다.
+    if(cnt == n) {
+        int total_cost = 0;
+        for(int i = 0; i < (int) picked.size() - 1; i++) {
+            int curr_cost = cost[picked[i]][picked[i + 1]];
+            // 만약 비용이 0이라면 불가능한 경우이므로 
+            // 바로 함수를 빠져나옵니다.
+            if(curr_cost == 0)
+                return;
+            
+            total_cost += curr_cost;
         }
-        cost += arr[v[i - 1]][v[i]];
-    }
-    v.pop_back();
-    //cout << cost << '\n';
-    return cost;
-}
 
-void simulate(int cnt){
-    if(cnt == a){
-        ans = min(ans, calc());
+        // 최종적으로 1번 지점으로 다시 돌아오는 것 까지 고려해서 계산해줍니다.
+        int additional_cost = cost[picked.back()][0];
+        if(additional_cost == 0)
+            return;
+
+        // 답을 갱신해줍니다.
+        ans = min(ans, total_cost + additional_cost);
         return;
     }
 
-    for(int i = 0; i < a; i++){
-        if(visited[i] == true){
-            continue;
-        }
-        if(v.size() > 0 && v[0] != 0){
-            continue;
-        }
-
+    // 방문할 지점을 선택합니다.
+    for(int i = 0; i < n; i++) {
+        if(visited[i]) continue;
         visited[i] = true;
-        v.push_back(i);
-        simulate(cnt + 1);
+        picked.push_back(i);
+
+        FindMin(cnt + 1);
+
         visited[i] = false;
-        v.pop_back();
+        picked.pop_back();
     }
 }
 
-
 int main() {
-    // 여기에 코드를 작성해주세요.
-    cin >> a;
-    for(int i = 0; i < a; i++){
-        for(int j = 0; j < a; j++){
-            cin >> arr[i][j];
-        }
-    }
-    simulate(0);
+    // 입력:
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> cost[i][j];
+
+    // 1번 지점을 시작으로
+    // 탐색을 진행합니다.
+    visited[0] = true;
+    picked.push_back(0);
+    FindMin(1);
+
     cout << ans;
     return 0;
 }
